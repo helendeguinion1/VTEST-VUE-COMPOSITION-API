cdb85110bc81acd02666aa868f214cc751b9b7c9
helendeguinion1 <helendeguinion@gmail.com>
2024-02-10 21:35:41 +0800

    Add toasts in App.vue, bind class per toast based on it's type

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
-  <div class="flex flex-col items-center justify-center w-screen h-screen gap-4">
-    <button class="bg-indigo-100 rounded-md border border-indigo-500 p-4" >
-      SHOW INFO TOAST
-    </button>
+  <div class="flex flex-col">
+     <div class="absolute h-full right-0 max-h-screen p-4 max-w-64 flex flex-col gap-4 overflow-y-auto overflow-x-hidden">
+      <div
+        v-for="(toast, i) in toastApi?.toasts.value"
+        :key="i" :class="toastClass(toast.type)">
+        {{ toast.message }}
+      </div>
+      </div>
+    <div class="flex flex-col items-center justify-center w-screen h-screen gap-4">
+      <button
+      class="bg-indigo-100 rounded-md border border-indigo-500 p-4"
+        @click="toastApi?.addToast('info', 'This is an info toast', 3000)">
+        SHOW INFO TOAST
+      </button>
 
-    <button class="bg-indigo-100 rounded-md border border-indigo-500 p-4" >
-      SHOW SUCCESS TOAST
-    </button>
+      <button
+        class="bg-indigo-100 rounded-md border border-indigo-500 p-4"
+        @click="toastApi?.addToast('success', 'This is an success toast', 3000)">
+        SHOW SUCCESS TOAST
+      </button>
 
-    <button class="bg-indigo-100 rounded-md border border-indigo-500 p-4" >
-      SHOW ERROR TOAST
-    </button>
+      <button
+        class="bg-indigo-100 rounded-md border border-indigo-500 p-4"
+        @click="toastApi?.addToast('error', 'This is an error toast', 3000)">
+        SHOW ERROR TOAST
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { useToast } from './composables/useToast';
-  const toastApi = useToast()
+  import { inject } from 'vue'
+  import { toastKey } from './constants/injectionKeys'
+  import { ToastType } from './types/Toast';
+
+  const toastApi = inject(toastKey);
+
+  const toastClass = (type: ToastType): string => {
+  switch (type) {
+    case 'info':
+      return 'p-3 rounded-lg border border-solid border-indigo-500 bg-indigo-100 w-full';
+    case 'success':
+      return 'p-3 rounded-lg border border-solid border-indigo-500 bg-indigo-100 w-full bg-green-100 border-green-500';
+    case 'error':
+      return 'p-3 rounded-lg border border-solid border-indigo-500 bg-indigo-100 w-full bg-red-100 border-red-500';
+    default:
+      return '';
+  }
+}
+
 </script>
