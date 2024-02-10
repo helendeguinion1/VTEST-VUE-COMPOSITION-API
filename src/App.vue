131e4fdb76bda4534b7f211c7ee0a7b242b1089f
helendeguinion1 <helendeguinion@gmail.com>
2024-02-10 15:34:03 +0800

    Add App.vue button design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <button>
+  <div class="flex flex-col items-center justify-center w-screen h-screen gap-4">
+    <button class="bg-indigo-100 rounded-md border border-indigo-500 p-4" >
       SHOW INFO TOAST
     </button>
 
-    <button>
+    <button class="bg-indigo-100 rounded-md border border-indigo-500 p-4" >
       SHOW SUCCESS TOAST
     </button>
 
-    <button>
+    <button class="bg-indigo-100 rounded-md border border-indigo-500 p-4" >
       SHOW ERROR TOAST
     </button>
   </div>
